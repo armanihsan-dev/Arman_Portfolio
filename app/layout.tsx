68cc2eb12e588baebf169d4ec52c7d578ae0ff9e
Arman Ihsan <armanihsan224@gmail.com>
2025-06-22 11:24:12 +0500

    delete reviewcard.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import './reviewCard.css';
 import StarsCanvas from '@/components/main/StarBackground';
 import Navbar from '@/components/main/Navbar';
 import Footer from '@/components/main/Footer';
